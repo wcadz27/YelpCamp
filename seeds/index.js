09cc53c7857cbc22d16a66277bb8caf1db269b9e
wcadz27 <wcadz27@gmail.com>
2021-12-24 15:35:58 -0800

    added CRUD functionality, boilerplate, partials, CSS bootstrap

---
+++ seeds/index.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+const Campground = require('../models/campground');
+const {places, descriptors} = require('./seedHelpers')
+const cities = require('./cities');
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+    useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+});
+
+const sample = array => array[Math.floor(Math.random() * array.length)];
+
+const seedDB = async () => {
+    await Campground.deleteMany({});
+    for(let i = 0; i < 50; i++) {
+        const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
+        const camp = new Campground({
+            location: `${cities[random1000].city}, ${cities[random1000].state}`,
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/483251/',
+            description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vitae excepturi, iure delectus autem voluptatem facere maxime eum inventore eligendi optio aliquid exercitationem illum alias? Laboriosam perspiciatis magnam modi ea quaerat?',
+            price
+            //or price:price
+        })
+        await camp.save();
+    }
+}
+
+seedDB().then(() => {
+    mongoose.connection.close();
+});
