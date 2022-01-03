4153f274f05f7cb383d119723cc7e14d6febae0a
wcadz27 <wcadz27@gmail.com>
2022-01-02 18:01:05 -0800

    added returnTo function which allows browswer to retain previous url after app redirects user to login page url

--- routes/users.js
+++ routes/users.js
@@ -32,6 +32,8 @@ router.get('/login', (req, res) => {
 
 router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
     req.flash('success', 'Welcome back');
+    const redirectUrl = req.session.returnTo || '/campgrounds';
+    delete req.session.returnTo;
     res.redirect('/campgrounds');
 })
 
