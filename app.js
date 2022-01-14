4e4c70b5f259f55f8deb30e0192c19c66c605f63
wcadz27 <wcadz27@gmail.com>
2022-01-14 10:15:17 -0800

    added cluster map to homepage

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
-if(process.env.NODE_ENV !== 'production'){
-    require('dotenv').config();
+if (process.env.NODE_ENV !== "production") {
+    require("dotenv").config();
 }
 
-const express = require('express');
-const path = require('path');
-const mongoose = require('mongoose');
-const ejsMate = require('ejs-mate');
-const session = require('express-session');
-const flash = require('express-flash');
-const ExpressError = require('./utilities/ExpressError')
-const methodOverride = require('method-override');
-const passport = require('passport');
-const LocalStrategy = require('passport-local');
-const User = require('./models/user');
+const express = require("express");
+const path = require("path");
+const mongoose = require("mongoose");
+const ejsMate = require("ejs-mate");
+const session = require("express-session");
+const flash = require("express-flash");
+const ExpressError = require("./utilities/ExpressError");
+const methodOverride = require("method-override");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user");
 
-const userRoutes = require('./routes/users')
-const campgroundsRoutes = require('./routes/campgrounds');
-const reviewsRoutes = require('./routes/reviews');
-const req = require('express/lib/request');
+const userRoutes = require("./routes/users");
+const campgroundsRoutes = require("./routes/campgrounds");
+const reviewsRoutes = require("./routes/reviews");
+const req = require("express/lib/request");
 
-mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+mongoose.connect("mongodb://localhost:27017/yelp-camp", {
     useUnifiedTopology: true,
 });
 
@@ -31,25 +31,25 @@ db.once("open", () => {
 
 const app = express();
 
-app.engine('ejs', ejsMate);
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'))
+app.engine("ejs", ejsMate);
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
-app.use(express.urlencoded({extended: true}));
-app.use(methodOverride('_method'))
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+app.use(express.static(path.join(__dirname, "public")));
 
 const sessionConfig = {
-    secret: 'thisshouldbeabettersecret!',
+    secret: "thisshouldbeabettersecret!",
     resave: false,
     saveUninitialized: true,
     cookie: {
         httpOnly: true,
         expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
-        maxAge: 1000 * 60 * 60 * 24 * 7
-    }
-}
-app.use(session(sessionConfig))
+        maxAge: 1000 * 60 * 60 * 24 * 7,
+    },
+};
+app.use(session(sessionConfig));
 app.use(flash());
 
 app.use(passport.initialize());
@@ -60,32 +60,30 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 app.use((req, res, next) => {
-    
     res.locals.currentUser = req.user;
-    res.locals.success = req.flash('success');
-    res.locals.error = req.flash('error');
+    res.locals.success = req.flash("success");
+    res.locals.error = req.flash("error");
     next();
-})
-
-app.use('/', userRoutes);
-app.use('/campgrounds', campgroundsRoutes);
-app.use('/campgrounds/:id/reviews', reviewsRoutes);
-
-
-app.get('/', (req, res) => {
-    res.render('home')
 });
 
-app.all('*', (req, res, next) => {
-    next(new ExpressError('Page Not Found', 404))
-})
+app.use("/", userRoutes);
+app.use("/campgrounds", campgroundsRoutes);
+app.use("/campgrounds/:id/reviews", reviewsRoutes);
+
+app.get("/", (req, res) => {
+    res.render("home");
+});
+
+app.all("*", (req, res, next) => {
+    next(new ExpressError("Page Not Found", 404));
+});
 
 app.use((err, req, res, next) => {
-    const { statusCode = 500, message = 'Something went wrong' } = err;
-    if(!err.message) err.message = 'Oh No, Something Went Wrong!'
-    res.status(statusCode).render('error', { err });
+    const { statusCode = 500, message = "Something went wrong" } = err;
+    if (!err.message) err.message = "Oh No, Something Went Wrong!";
+    res.status(statusCode).render("error", { err });
 });
 
 app.listen(3000, () => {
-    console.log('Serving on port 3000!')
+    console.log("Serving on port 3000!");
 });
--- seeds/index.js
+++ seeds/index.js
@@ -1,10 +1,10 @@
-const mongoose = require('mongoose');
-const Campground = require('../models/campground');
-const {places, descriptors} = require('./seedHelpers')
-const cities = require('./cities');
+const mongoose = require("mongoose");
+const Campground = require("../models/campground");
+const { places, descriptors } = require("./seedHelpers");
+const cities = require("./cities");
 
-mongoose.connect('mongodb://localhost:27017/yelp-camp', {
-    useUnifiedTopology: true
+mongoose.connect("mongodb://localhost:27017/yelp-camp", {
+    useUnifiedTopology: true,
 });
 
 const db = mongoose.connection;
@@ -17,34 +17,38 @@ const sample = array => array[Math.floor(Math.random() * array.length)];
 
 const seedDB = async () => {
     await Campground.deleteMany({});
-    for(let i = 0; i < 50; i++) {
+    for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             //USER ID
-            author: '61d0eee5eb1c2013e5afcaca',
+            author: "61d0eeSe5eb1c2013e5afcaca",
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
-            description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vitae excepturi, iure delectus autem voluptatem facere maxime eum inventore eligendi optio aliquid exercitationem illum alias? Laboriosam perspiciatis magnam modi ea quaerat?',
+            description:
+                "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vitae excepturi, iure delectus autem voluptatem facere maxime eum inventore eligendi optio aliquid exercitationem illum alias? Laboriosam perspiciatis magnam modi ea quaerat?",
             price,
-            image: [
-                {
-                  url: 'https://res.cloudinary.com/dyr50c85v/image/upload/v1641561799/YelpCamp/px2lv9butjgzyu4omtpk.jpg',
-                  filename: 'YelpCamp/px2lv9butjgzyu4omtpk'
-                },
-                {
-                  url: 'https://res.cloudinary.com/dyr50c85v/image/upload/v1641561799/YelpCamp/uxlttmaprkxqkhcelgzj.jpg',
-                  filename: 'YelpCamp/uxlttmaprkxqkhcelgzj'
-                }
-              ],
             geometry: {
                 type: "Point",
-                coordinates: [-113.1331, 47.0202]
-            }
-        })
+                coordinates: [
+                    cities[random1000].longitude,
+                    cities[random1000].latitude,
+                ],
+            },
+            image: [
+                {
+                    url: "https://res.cloudinary.com/dyr50c85v/image/upload/v1641561799/YelpCamp/px2lv9butjgzyu4omtpk.jpg",
+                    filename: "YelpCamp/px2lv9butjgzyu4omtpk",
+                },
+                {
+                    url: "https://res.cloudinary.com/dyr50c85v/image/upload/v1641561799/YelpCamp/uxlttmaprkxqkhcelgzj.jpg",
+                    filename: "YelpCamp/uxlttmaprkxqkhcelgzj",
+                },
+            ],
+        });
         await camp.save();
     }
-}
+};
 
 seedDB().then(() => {
     mongoose.connection.close();
